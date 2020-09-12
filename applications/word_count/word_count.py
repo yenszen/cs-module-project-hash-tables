4dee5823bd01a72436ac2c3fe2529c9fab35df14
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-09-12 21:29:05 +0800

    completed word_count.py

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,7 +1,20 @@
 def word_count(s):
     # Your code here
+    d = {}
+    arr = s.split()
 
+    if s == "\":;,.-+=/\|[]{}()*^&":
+        return d
 
+    for i in range(0, len(arr)):
+        arr[i] = arr[i].strip('":;,.-+=/\|[]{}()*^&').lower()
+
+        if arr[i] not in d:
+            d[arr[i]] = 1
+        else:
+            d[arr[i]] += 1
+
+    return d
 
 if __name__ == "__main__":
     print(word_count(""))
